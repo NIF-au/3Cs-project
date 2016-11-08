50d47010558202ac786e64d1a2e08adb9f893b00
Christos <xzoupis@gmail.com>
2016-11-08 15:04:01 +1000

    oh yeah

--- code/mri2ct.m
+++ code/mri2ct.m
@@ -1,13 +1,13 @@
 function mri2ct()
 
 % Catharina Maria Hamer Holland - holland.cat@hotmail.com
-% Christoffer GÃ¸thgen - cgathg11@student.aau.dk
+% Christoffer Gøthgen - cgathg11@student.aau.dk
 % Christos Zoupis Schoinas - xzoupis@gmail.com
 % Andrew Janke - a.janke@gmail.com
 % 
 % Copyright 
 % Catharina Maria Hamer Holland, Aalborg University.
-% Christoffer GÃ¸thgen, Aalborg University.
+% Christoffer Gøthgen, Aalborg University.
 % Christos Zoupis Schoinas, Aalborg University.
 % Andrew Janke, The University of Queensland.
 % Permission to use, copy, modify, and distribute this software and its
@@ -18,13 +18,13 @@ function mri2ct()
 % without express or implied warranty.
 
 PATH = getenv('PATH');
-if ~strfind(PATH, 'data/lfs2/software/ubuntu14/minc-itk4-1.9.11-20160202/bin')
-    setenv('PATH', [PATH ':/data/lfs2/software/ubuntu14/minc-itk4-1.9.11-20160202/bin']);
+if isempty(strfind(PATH, 'data/lfs2/software/ubuntu14/minc-itk4-1.9.11-20160202/bin'))
+    setenv('PATH', ['/data/lfs2/software/ubuntu14/minc-itk4-1.9.11-20160202/bin:' PATH]);
 end
 
 LIB = getenv('LD_LIBRARY_PATH');
-if ~strfind(LIB, 'data/lfs2/software/ubuntu14/minc-itk4-1.9.11-20160202/lib')
-    setenv('LD_LIBRARY_PATH', [LIB ':/data/lfs2/software/ubuntu14/minc-itk4-1.9.11-20160202/lib']);
+if isempty(strfind(LIB, 'data/lfs2/software/ubuntu14/minc-itk4-1.9.11-20160202/lib'))
+    setenv('LD_LIBRARY_PATH', ['/data/lfs2/software/ubuntu14/minc-itk4-1.9.11-20160202/lib:' LIB]);
 end
 
 
@@ -34,9 +34,9 @@ fprintf('Select the NifTi file with the CT images\n')
 fprintf('Select the NifTi file with the MR images\n')
 [MRIName,mripath] = uigetfile('*.nii','Select the  NifTi file with the MR images');
 
-dos(['export LD_LIBRARY_PATH=/data/lfs2/software/ubuntu14/minc-itk4-1.9.11-20160202/bin/:$LD_LIBRARY_PATH; nii2mnc ' fullfile(ctpath, CTName) ' CTmnc.mnc']);
+dos(['nii2mnc ' fullfile(ctpath, CTName) ' CTmnc.mnc']);
 
-dos(['export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH; nii2mnc ' fullfile(mripath, MRIName) ' MRImnc.mnc']);
+dos(['nii2mnc ' fullfile(mripath, MRIName) ' MRImnc.mnc']);
 
 dos('register MRImnc.mnc CTmnc.mnc');
 
@@ -44,10 +44,10 @@ dos('register MRImnc.mnc CTmnc.mnc');
 fprintf('Select the NifTi file with the tr images\n')
 [trName,trpath] = uigetfile('*','Select the  NifTi file with the tr images');
 
-dos(['export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH; mincresample -like MRImnc.mnc -transformation ' fullfile(trpath, trName), ...
+dos(['mincresample -like MRImnc.mnc -transformation ' fullfile(trpath, trName), ...
     ' CTmnc.mnc after_resample.mnc']);
 
-dos('export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH; mnc2nii after_resample.mnc NewCT.mnc');
+dos('mnc2nii after_resample.mnc NewCT.mnc');
 
 ct = load_nii(fullfile(ctpath, CTName));
 img = ct.img;
@@ -79,14 +79,14 @@ vox3 = vox(3);
 nii_file = make_nii(array3d, [vox1, vox2, vox3]);
 save_nii(nii_file, 'CTmask.nii')
 
-dos('export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH; nii2mnc CTmask.nii CTmask_mnc.mnc');
+dos('nii2mnc CTmask.nii CTmask_mnc.mnc');
 
 dos('register MRImnc.mnc CTmask_mnc.mnc');
 
 fprintf('Select the NifTi file with the MR images\n')
 [tr2Name,tr2path] = uigetfile('*','Select the  NifTi file with the MR images');
 
-dos(['export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH; mincresample -like MRImnc.mnc -transformation ' fullfile(tr2path, tr2Name), ...
+dos(['mincresample -like MRImnc.mnc -transformation ' fullfile(tr2path, tr2Name), ...
     ' CTmask_mnc.mnc after_2resample.mnc']);
 
-dos('export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH; mnc2nii after_2resample.mnc NewCT2.mnc');
+dos('mnc2nii after_2resample.mnc NewCT2.mnc');
