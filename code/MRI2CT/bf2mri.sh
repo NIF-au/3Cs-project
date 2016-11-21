537bc243b28a3c061590de8370add2c3cd7a4fc5
Christos <xzoupis@gmail.com>
2016-11-21 15:05:55 +1000

    Update bf2mri.sh

--- code/MRI2CT/bf2mri.sh
+++ code/MRI2CT/bf2mri.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Catharina Maria Hamer Holland - holland.cat@hotmail.com
+# Christoffer Gøthgen - cgathg11@student.aau.dk
+# Christos Zoupis Schoinas - xzoupis@gmail.com
+# Andrew Janke - a.janke@gmail.com
+# 
+# Copyright 
+# Catharina Maria Hamer Holland, Aalborg University.
+# Christoffer Gøthgen, Aalborg University.
+# Christos Zoupis Schoinas, Aalborg University.
+# Andrew Janke, The University of Queensland.
+# Permission to use, copy, modify, and distribute this software and its
+# documentation for any purpose and without fee is hereby granted,
+# provided that the above copyright notice appear in all copies.  The
+# authors and the Universities make no representations about the
+# suitability of this software for any purpose.  It is provided "as is"
+# without express or implied warranty.
+
 choice=0
 out_dir=" "
 inputsOK=(0 0 0 0 0)
