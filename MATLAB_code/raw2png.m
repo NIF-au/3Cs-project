e62555dc4a3bf994edcea1854abd083d0ab3eb99
Christos <xzoupis@gmail.com>
2016-11-23 09:26:03 +1000

    Rename code/raw2png.m to MATLAB_code/raw2png.m

--- MATLAB_code/raw2png.m
+++ MATLAB_code/raw2png.m
@@ -6,13 +6,13 @@ function raw2png(Dir, extension)
 % here: http://www.imagemagick.org/script/binary-releases.php 
 
 % Catharina Maria Hamer Holland - holland.cat@hotmail.com
-% Christoffer Gøthgen - cgathg11@student.aau.dk
+% Christoffer GÃ¸thgen - cgathg11@student.aau.dk
 % Christos Zoupis Schoinas - xzoupis@gmail.com
 % Andrew Janke - a.janke@gmail.com
 % 
 % Copyright 
 % Catharina Maria Hamer Holland, Aalborg University.
-% Christoffer Gøthgen, Aalborg University.
+% Christoffer GÃ¸thgen, Aalborg University.
 % Christos Zoupis Schoinas, Aalborg University.
 % Andrew Janke, The University of Queensland.
 % Permission to use, copy, modify, and distribute this software and its
