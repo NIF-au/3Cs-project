de1d3e46ac70506d4c4c15f40448810827ee0b3d
Christos <xzoupis@gmail.com>
2016-11-24 17:23:47 +1000

    Update

--- bf2mri.sh
+++ bf2mri.sh
@@ -32,11 +32,11 @@ do
         do
           if [ ${inputsOK[i]} -eq 0 ]
           then
-              echo ${menu[i]}
+			echo ${menu[i]}
 
           else
 
-              echo "${menu[i]} ..........ok"
+			echo "${menu[i]} ..........ok"
 
           fi
         done
@@ -48,60 +48,60 @@ do
 
         1)
 
-                if [[ $out_dir != *[!\ ]* ]]
-                then
-                        out_dir=$(zenity --file-selection --directory --title="Select the path for the output files" --filename=pwd)
-                fi
-                echo "Enter the Transformation Matrix name"
-                read TMN
-                if [[ $out_dir = *[!\ ]* ]]  &&  [[ $TMN = *[!\ ]* ]]
-                then
-                        inputsOK[0]=1
-                fi;;
+			if [[ $out_dir != *[!\ ]* ]]
+			then
+				out_dir=$(zenity --file-selection --directory --title="Select the path for the output files" --filename=pwd)
+			fi
+			echo "Enter the Transformation Matrix name"
+			read TMN
+			if [[ $out_dir = *[!\ ]* ]]  &&  [[ $TMN = *[!\ ]* ]]
+			then
+				inputsOK[0]=1
+			fi;;
 
         2)
 
-                if [[ $out_dir != *[!\ ]* ]]
-                then
-                        out_dir=$(zenity --file-selection --directory --title="Select the path for the output files" --filename=pwd)
-                fi
-                echo "Enter the name for the registered image name (don't forget the .nii in the end) "
-                read RIN
-                if [[ $out_dir = *[!\ ]* ]]  &&  [[ $RIN = *[!\ ]* ]]
-                then
-                        inputsOK[1]=1
-                fi;;
+			if [[ $out_dir != *[!\ ]* ]]
+			then
+				out_dir=$(zenity --file-selection --directory --title="Select the path for the output files" --filename=pwd)
+			fi
+			echo "Enter the name for the registered image name (don't forget the .nii in the end) "
+			read RIN
+			if [[ $out_dir = *[!\ ]* ]]  &&  [[ $RIN = *[!\ ]* ]]
+			then
+				inputsOK[1]=1
+			fi;;
 
         3)
 
-                method=$(zenity --list --width=500 --height=250 --title="Select Transformation method" --column="Methods" Rigid Affine SyN)
-                if [[ $method = *[!\ ]* ]]
-                then
-                        inputsOK[2]=1
-                fi;;
+			method=$(zenity --list --width=500 --height=250 --title="Select Transformation method" --column="Methods" Rigid Affine SyN)
+			if [[ $method = *[!\ ]* ]]
+			then
+				inputsOK[2]=1
+			fi;;
         4)
 
-                fixed=$(zenity --file-selection --file-filter='*.nii' --title="Select the fixed images" --filename=pwd)
-                if [[ $fixed = *[!\ ]* ]]
-                then
-                        inputsOK[3]=1
-                fi;;
+			fixed=$(zenity --file-selection --file-filter='*.nii' --title="Select the fixed images" --filename=pwd)
+			if [[ $fixed = *[!\ ]* ]]
+			then
+				inputsOK[3]=1
+			fi;;
 
         5)
 
-                moving=$(zenity --file-selection --file-filter='*.nii' --title="Select the moving images" --filename=pwd)
-                if [[ $moving = *[!\ ]* ]]
-                then
-                        inputsOK[4]=1
-                fi;;
+			moving=$(zenity --file-selection --file-filter='*.nii' --title="Select the moving images" --filename=pwd)
+			if [[ $moving = *[!\ ]* ]]
+			then
+				inputsOK[4]=1
+			fi;;
 
         6)
 
-                echo Cheers!;;
+			echo Cheers!;;
 
         *)
 
-                echo "Wrong Choice, try again";;
+			echo "Wrong Choice, try again";;
         esac
 
 done
@@ -110,14 +110,14 @@ if [ $choice -ne 6]
 then
         if [ $method -eq 'SyN']
         then
-	        parameters=$(zenity --forms --title="Add Parameters" --text="Remember not to use spaces after commas!" --separator="," --add-entry=$'Dimensionality\n(eg. 3)' --add-entry=$'float\n(eg. 0)' --add-entry=$'Interpolation\n(eg. Linear)' --add-entry=$'winsorize image intensities\n(eg. 0.005,0.995)' --add-entry=$'Use histogram matching\n(eg. 0)' --add-entry=$'Num_transform\n(eg. 0.1,3,0)' --add-entry=$'Convergence\n(eg. 100,70,50,20,1e-6,10)' --add-entry=$'Metric\n(eg. 1,4)' --add-entry=$'Shrink Factors\n(eg. 8,4,2,1)' --add-entry=$'Smoothing sigmas\n(eg. 3,2,1,0)')
-
-                antsRegistration --dimensionality ${par[1]} --float ${par[2]} --output [$out_dir/$TMN,$out_dir/RIN] --interpolation ${par[3]} --winsorize-image-intensities [${par[4]}, ${par[5]}] --use-histogram-matching ${par[6]} --initial-moving-transform [$fixed, $moving, 1] --transform $method[${par[6]}, ${par[7]}, ${par[8]}] --metric CC[$fixed,$moving,${par[9]},${par[10]}] --convergence [${par[11]}x${par[12]}x${par[13]}x${par[14]},${par[15]},${par[16]}] --shrink-factors ${par[17]}x${par[18]}x${par[19]}x${par[20]} --smoothing-sigmas ${par[21]}x${par[22]}x${par[22]}x${par[23]}vox --verbose
+	        parameters=$(zenity --forms --title="Add Parameters" --text="Remember not to use spaces after commas!" --separator="," --add-entry=$'Dimensionality\n(eg. 3)' --add-entry=$'float\n(eg. 0)' --add-entry=$'Interpolation\n(eg. Linear)' --add-entry=$'winsorize image intensities\n(eg. 0.005,0.995)' --add-entry=$'Use histogram matching\n(eg. 0)' --add-entry=$'Num_transform\n(eg. 0.1,3,0)' --add-entry=$'Metric\n(eg. 1,4)' --add-entry=$'Convergence\n(eg. 100,70,50,20,1e-6,10)' --add-entry=$'Shrink Factors\n(eg. 8,4,2,1)' --add-entry=$'Smoothing sigmas\n(eg. 3,2,1,0)')
+			par=(${Parameters//,/ })
+            antsRegistration --dimensionality ${par[1]} --float ${par[2]} --output [$out_dir/$TMN,$out_dir/RIN] --interpolation ${par[3]} --winsorize-image-intensities [${par[4]}, ${par[5]}] --use-histogram-matching ${par[6]} --initial-moving-transform [$fixed, $moving, 1] --transform $method[${par[6]}, ${par[7]}, ${par[8]}] --metric CC[$fixed,$moving,${par[9]},${par[10]}] --convergence [${par[11]}x${par[12]}x${par[13]}x${par[14]},${par[15]},${par[16]}] --shrink-factors ${par[17]}x${par[18]}x${par[19]}x${par[20]} --smoothing-sigmas ${par[21]}x${par[22]}x${par[22]}x${par[23]}vox --verbose
 
         else
-                parameters=$(zenity --forms --title="Add Parameters" --text="Remember not to use spaces after commas!" --separator="," --add-entry=$'Dimensionality\n(eg. 3)' --add-entry=$'float\n(eg. 0)' --add-entry=$'Interpolation\n(eg. Linear)' --add-entry=$'winsorize image intensities\n(eg. 0.005,0.995)' --add-entry=$'Use histogram matching\n(eg. 0)' --add-entry=$'Step size\n(eg. 0.1)' --add-entry=$'Convergence\n(eg. 1000,500,250,100,1e-6,10)' --add-entry=$'Metric\n(eg. 1,4)' --add-entry=$'Shrink Factors\n(eg. 8,4,2,1)' --add-entry=$'Smoothing sigmas\n(eg. 3,2,1,0)')
-
-                antsRegistration --dimensionality ${par[1]} --float ${par[2]} --output [$out_dir/$TMN,$out_dir/RIN] --interpolation ${par[3]} --winsorize-image-intensities [${par[4]}, ${par[5]}] --use-histogram-matching ${par[6]} --initial-moving-transform [$fixed, $moving, 1] --transform $method[${par[6]}] --metric MI[$fixed,$moving,${par[7]},${par[8]},${par[9]},${par[10]}] --convergence [${par[11]}x${par[12]}x${par[13]}x${par[14]},${par[15]},${par[16]}] --shrink-factors ${par[17]}x${par[18]}x${par[19]}x${par[20]} --smoothing-sigmas ${par[21]}x${par[22]}x${par[22]}x${par[23]}vox --verbose
+            parameters=$(zenity --forms --title="Add Parameters" --text="Remember not to use spaces after commas!" --separator="," --add-entry=$'Dimensionality\n(eg. 3)' --add-entry=$'float\n(eg. 0)' --add-entry=$'Interpolation\n(eg. Linear)' --add-entry=$'winsorize image intensities\n(eg. 0.005,0.995)' --add-entry=$'Use histogram matching\n(eg. 0)' --add-entry=$'Step size\n(eg. 0.1)' --add-entry=$'Metric\n(eg. 1 32 Regular 0.25)' --add-entry=$'Convergence\n(eg. 1000,500,250,100,1e-6,10)' --add-entry=$'Shrink Factors\n(eg. 8,4,2,1)' --add-entry=$'Smoothing sigmas\n(eg. 3,2,1,0)')
+			par=(${parameters//,/ })
+            antsRegistration --dimensionality ${par[1]} --float ${par[2]} --output [$out_dir/$TMN,$out_dir/RIN] --interpolation ${par[3]} --winsorize-image-intensities [${par[4]}, ${par[5]}] --use-histogram-matching ${par[6]} --initial-moving-transform [$fixed, $moving, 1] --transform $method[${par[6]}] --metric MI[$fixed,$moving,${par[7]},${par[8]},${par[9]},${par[10]}] --convergence [${par[11]}x${par[12]}x${par[13]}x${par[14]},${par[15]},${par[16]}] --shrink-factors ${par[17]}x${par[18]}x${par[19]}x${par[20]} --smoothing-sigmas ${par[21]}x${par[22]}x${par[22]}x${par[23]}vox --verbose
         fi
 fi
 
